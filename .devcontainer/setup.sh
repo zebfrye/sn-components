36cf29e8723a7e345f0027060149db9a06283fd0
Zeb Frye <zeb@fryedesign.com>
2026-02-20 06:57:03 +0000

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,10 +6,10 @@ curl -L https://github.com/ServiceNow/servicenow-cli/releases/download/v1.1.3/sn
 
 # Unzip
 cd /tmp
-unzip snc.zip
+unzip -o /tmp/snc.zip -d /tmp/snc-extract
 
 # Run the Linux x64 installer
-chmod +x /tmp/snc-1.1.3/snc-1.1.3-linux-x64-installer.run
-sudo /tmp/snc-1.1.3/snc-1.1.3-linux-x64-installer.run --mode unattended
+chmod +x /tmp/snc-extract/snc-1.1.3-linux-x64-installer.run
+sudo /tmp/snc-extract/snc-1.1.3-linux-x64-installer.run --mode unattended
 
 echo "ServiceNow CLI installed: $(snc --version)"
